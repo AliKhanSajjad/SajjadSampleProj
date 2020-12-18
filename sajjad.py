93db10877ad07613cc0f4697ff9cdf8c626fa6b6
sajjkhan <sajjadalikhan.ssit@gmail.com>
2020-12-18 14:53:47 +0530

    Python revision 1 from my pc

---
+++ sajjad.py
@@ -0,0 +1,29 @@
+"""
+Using docstring as a comment.
+This code divides 2 numbers
+"""
+
+import numpy as np
+from random import random,shuffle
+import re
+
+arr1 = np.array([1,2,4,5])
+
+shuffle(arr1)
+print(arr1)
+
+for i in range(10):
+    print(i)
+
+dict={'Country':'India','Capital':'Delhi','PM':'Modi'}
+
+print(dict['Country'])
+
+len(dict)
+
+
+str="Sajad ali khan"
+
+import os
+
+
